1477196c569220622c245ae1657a9feae81633f1
Marcos Perez <perezmarcos04@yahoo.com.ar>
2025-03-08 20:04:30 -0300

    Codigo ya funcional. Se elimiaron los helpers del tutorial y se eliminaron las variables de entorno que definen los ARN de S3

--- L3_Enable_logging.py
+++ L3_Enable_logging.py
@@ -9,8 +9,6 @@
 # ### Import all needed packages
 import boto3
 import json
-import os
-from helpers.CloudWatchHelper import CloudWatch_Helper
 
 # Recordar que Cloudwatch es un servicio de AWS que se encarga de crear logs y métricas del estado
 # de salud de los servicios que utilizamos. Desde luego los logs los podemos ver en la consola de AWS también.
@@ -21,34 +19,35 @@ session = boto3.Session(profile_name = "AdministratorAccess-376129873205")
 # generar texto sino para evaluar el servicio mismo de Bedrock (para crear los logs)
 bedrock = session.client('bedrock', region_name="us-east-1")
 
-cloudwatch = CloudWatch_Helper()
-
 # Dentro de CloudWatch existen grupos de logs y el siguiente código lo que hace es crear un grupo para nuestros registros.
 # Tener en cuenta que la ubicación que se ve abajo fue sugerida por el instructor del tutorial y la misma tiene seteado
-# permisos en la # cuenta de AWS de Deeplearning por lo que, sea cual sea nuestra ubicación de logs, tenemos que setearle
-# permisos también. # Estos permisos son: IAM. Mirando el JSON de permisos se ve que tiene los keys:
-# "CreateLogStreams" y "PutLogEvents". # Luego el resource que tiene permiso para esto es Amazon Bedrock model invokations.
+# permisos en la cuenta de AWS de Deeplearning por lo que, sea cual sea nuestra ubicación de logs, tenemos que setearle
+# permisos también. Estos permisos son: IAM. Mirando el JSON de permisos se ve que tiene los keys:
+# "CreateLogStreams" y "PutLogEvents". Luego el resource que tiene permiso para esto es Amazon Bedrock model invokations.
 # También tienen seteado un Trust Policy que puede ser asumida por  el servicio de Bedrock
+
+cloudwatch_client = session.client('logs', region_name="us-east-1")
 
 # String con la ubicación de los logs
 log_group_name = '/my/amazon/bedrock/logs'
 
-# Invoco al servicio de CloudWatch para que genere dicha ubicación
-cloudwatch.create_log_group(log_group_name)
-
-# Todo lo anterior se puede hacer igualmente desde la consola de AWS.
+try:
+    response = cloudwatch_client.create_log_group(logGroupName=log_group_name)
+    print("Log group created successfully:", response)
+except Exception as e:
+    print("Failed to create log group:", e)
 
 loggingConfig = {
     'cloudWatchConfig': {
         'logGroupName': log_group_name,
-        'roleArn': os.environ['LOGGINGROLEARN'],
+        'roleArn': 'arn:aws:iam::376129873205:role/RolParaLogsDeAmazonBedrock',
         'largeDataDeliveryS3Config': {
-            'bucketName': os.environ['LOGGINGBUCKETNAME'],
+            'bucketName': 'bucket-para-archivos-mp3-20250202',
             'keyPrefix': 'amazon_bedrock_large_data_delivery',
         }
     },
     's3Config': {
-        'bucketName': os.environ['LOGGINGBUCKETNAME'],
+        'bucketName': 'bucket-para-archivos-mp3-20250202',
         'keyPrefix': 'amazon_bedrock_logs',
     },
     'textDataDeliveryEnabled': True,
@@ -58,9 +57,9 @@ bedrock.put_model_invocation_logging_configuration(loggingConfig=loggingConfig)
 bedrock.get_model_invocation_logging_configuration()
 
 # Ahora sí vamos a crear un cliente del runtime de Bedrock
-bedrock_runtime = boto3.session('bedrock-runtime', region_name="us-east-1")
+bedrock_runtime = session.client('bedrock-runtime', region_name="us-east-1")
 
-prompt = "Write an article about the fictional planet Foobar."
+prompt = "Write one line description of Buenos Aires."
 
 kwargs = {
     "modelId": "amazon.titan-text-lite-v1",
@@ -82,9 +81,3 @@ response = bedrock_runtime.invoke_model(**kwargs)
 response_body = json.loads(response.get('body').read())
 generation = response_body['results'][0]['outputText']
 print(generation)
-
-cloudwatch.print_recent_logs(log_group_name)
-
-# Mostrar un link para ir a revisar los logs generados en la consola de AWS
-aws_url = os.environ.get('AWS_CONSOLE_URL', 'https://aws.amazon.com/console/')
-print(f"GO TO AWS CONSOLE: {aws_url}")
